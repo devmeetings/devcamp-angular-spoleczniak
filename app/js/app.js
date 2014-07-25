3a880c1c338e298b3330637a668e81c1d6700057
Grzegorz Misiołek <grzegorz.misiolek@gmail.com>
2014-07-25 12:00:39 +0200

    vendor and libraries

---
+++ app/js/app.js
@@ -0,0 +1,44 @@
+/*global angular, templates_paths, Modernizr */
+
+var spolApp, spolFilters, spolControllers, spolServices, spolDirectives;
+
+
+/**
+ * Create application modules.
+ */
+spolApp = angular.module('app', ['app.filters', 'app.controllers', 'app.services', 'app.directives','ngRoute']);
+spolFilters = angular.module('app.filters', []);
+spolControllers = angular.module('app.controllers', []);
+spolServices = angular.module('app.services', []);
+spolDirectives = angular.module('app.directives', []);
+
+
+/**
+ * Setting router provider config.
+ */
+spolApp.config(['$routeProvider', '$httpProvider', '$locationProvider',
+    function ($routeProvider, $httpProvider, $locationProvider) {
+
+        'use strict';
+
+        $routeProvider
+            // homepage url - searcher
+            .when('/dashboard', {
+                templateUrl: templates_paths.page_dashboard
+            })
+            .when('/login', {
+                templateUrl: templates_paths.page_login
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
+
+        // change default content-type for $http.post request
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+        // add header for ajax visibility.
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        $httpProvider.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
+
+        $locationProvider.html5Mode(false).hashPrefix('!');
+    }]);
