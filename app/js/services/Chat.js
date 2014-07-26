bbe261c79f0343dbebb944e414afe9257dfa75ce
Grzegorz Misiołek <grzegorz.misiolek@gmail.com>
2014-07-26 11:54:16 +0200

    dodanie modelu chat

---
+++ app/js/services/Chat.js
@@ -0,0 +1,40 @@
+spolServices
+    .factory('Chat', ['$goKey', '$location', '$goQuery', 'loginService', function ($goKey, $location, $goQuery, loginService) {
+
+        var new_message;
+
+        new_message = {
+            idmessage: null,                 // id wiadomości
+            iduser: null,           // id użytkownika
+            text: null,            // treść komentarza
+            date: 0           // czas dodania
+        };
+
+        return {
+            chat: null,
+
+            getMessages: function () {
+                var id = loginService.getLoggedInId();
+                this.chat = $goQuery('chat', { iduser: id });
+                this.chat.$sync();
+
+                return this.chat;
+            },
+
+            addMessage: function (data) {
+                var id, date;
+                id = loginService.getLoggedInId();
+                date = new Date();
+
+                data.idmessage = new Date().getTime();
+                data.date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ', ' + date.getHours() + ':' + date.getMinutes();
+                data.iduser = id;
+
+                this.chat.$add(data);
+            },
+
+            getBlankComment: function () {
+                return Object.create(new_message);
+            }
+        }
+    }]);
