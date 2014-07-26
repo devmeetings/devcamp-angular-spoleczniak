51f8312bfa649f3721dede6d1450e8382c675a45
Grzegorz Misiołek <grzegorz.misiolek@gmail.com>
2014-07-26 13:00:01 +0200

    pokazywanie bledu gdy za krotka wiadomosc na chacie;

--- app/js/controllers/ChatController.js
+++ app/js/controllers/ChatController.js
@@ -24,12 +24,17 @@ spolControllers.controller("chatController", ["$scope", "Chat", "loginService", 
         $scope.chatItems.$on("ready", function() {
         	$scope.chatIsLoaded = true;
         });
-
+        $scope.show_error = false;
         $scope.newMessage = Chat.getBlankMessage();
         $scope.sendMessage = function() {
-        	if ($scope.newMessage.text.length < 5) return;
-        	Chat.addMessage($scope.newMessage);
-        	$scope.newMessage = Chat.getBlankMessage();
+        	if ($scope.newMessage.text === null || $scope.newMessage.text.length < 5) {
+                $scope.message_error = "wiadomość musi mieć co najmniej 5 znaków";
+                $scope.show_error = true;
+            } else {
+                $scope.show_error = false;
+                Chat.addMessage($scope.newMessage);
+                $scope.newMessage = Chat.getBlankMessage();
+            }
 
         };
 
