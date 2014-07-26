9caf42f7d476ce857429c29d657aa56c123d4930
Grzegorz Misiołek <grzegorz.misiolek@gmail.com>
2014-07-26 09:13:37 +0200

    poprawa wylogowywania

--- app/js/controllers/IndexController.js
+++ app/js/controllers/IndexController.js
@@ -1,8 +1,9 @@
 spolControllers.controller("indexController", function($scope, loginService, $rootScope) {
 		$scope.loggedin = loginService.getLoggedInId() ? 1 : 0;
-		$scope.logout = function() {
+		$scope.logout = function($event) {
 			loginService.doLogout();
 			$scope.loggedin = 0;
+            $event.preventDefault();
 		};
 		$rootScope.$on("doLogin", function() {
 			$scope.loggedin = 1;
